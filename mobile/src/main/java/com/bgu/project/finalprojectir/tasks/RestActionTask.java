6fc1fb3b9cab3b70d004e19343007b6b01351be2
Boaz <boaz225@gmail.com>
2015-06-20 19:19:48 +0300

    fix for REST request when there is not response

--- mobile/src/main/java/com/bgu/project/finalprojectir/tasks/RestActionTask.java
+++ mobile/src/main/java/com/bgu/project/finalprojectir/tasks/RestActionTask.java
@@ -20,7 +20,13 @@ public class RestActionTask extends AbstractRestTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... params) {
-        return restTemplate.getForObject(uri, String.class);
+        String responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForObject(uri, String.class);
+        } catch (Exception e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+        return responseEntity;
     }
 
 }
